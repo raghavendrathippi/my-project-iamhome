e3f06b7739fa3c53abee705c644ba1859cca5c89
IamHomeProject@gmail.com <IamHomeProject@gmail.com@1ec1ea51-4dc7-0de0-3faf-29cb2834436d>
2011-06-04 06:53:49 +0000

    Update ProximitySettings

--- trunk/IamHomeProject/src/com/TigerLee/HomeIn/ProximityAlertPreferencesActivity.java
+++ trunk/IamHomeProject/src/com/TigerLee/HomeIn/ProximityAlertPreferencesActivity.java
@@ -6,10 +6,8 @@ import android.preference.PreferenceActivity;
 public class ProximityAlertPreferencesActivity extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.settings);
-
 	}
 
 }
